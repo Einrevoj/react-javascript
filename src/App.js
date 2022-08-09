1c86307f4d5d8f23e7a618505d3ff753e1acec1c
Einrevoj <jovcano28@gmail.com>
2022-08-09 21:07:53 +0800

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-        <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path=''
+    </Routes>
   );
 }
 
