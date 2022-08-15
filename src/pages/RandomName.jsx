1e8f4542a3016a4bcb2567674e3b14f4b9ae4c92
Einrevoj <jovcano28@gmail.com>
2022-08-15 19:41:46 +0800

    added page

---
+++ src/pages/RandomName.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+export default function RandomName() {
+  const [outPut, setOutput] = useState("");
+  const names = [
+    "Alex",
+    "Pearl",
+    "Aries",
+    "Paulo",
+    "Jeffrey",
+    "John Glay",
+    "John Paul",
+    "Jovernie",
+    "Kaila",
+    "Kevin",
+    "Lorenz",
+    "Marianne",
+    "Mark",
+    "Matthew",
+    "Michael",
+    "Nathan",
+    "Bong",
+    "Rhea",
+    "Saro",
+    "Adrianne",
+    "Beverly",
+    "Ray Matthew",
+    "Therese",
+    "Joeneil",
+    "Richard",
+    "Norman",
+  ];
+
+  const getName = () => {
+    const index = Math.floor(Math.random() * 26);
+    return setOutput(names[index]);
+  };
+
+
+return (
+  <div className="container p-5">
+    <div className="row">
+      <div className="col-12 w-50 pb-5">
+        <button className="btn btn-warning w-50 mb-5" onClick={() => getName()}>
+          Generate Name
+        </button>
+        <h1 className="text-primary" w-50 mb-5>
+          {outPut}
+        </h1>
+      </div>
+    </div>
+  </div>
+);
+}
